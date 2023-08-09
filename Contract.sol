7ad4be7865c37b78fdb43d72ef6f58879c116b8e
Valda <valda.bimbirule1@gmail.com>
2023-08-09 11:46:18 +0300

    Transferring ownership

--- Contract.sol
+++ Contract.sol
@@ -2,17 +2,40 @@ pragma solidity ^0.8.6;
 
 abstract contract ERC20Token{
 
-function name() public view returns (string)
-function symbol() public view returns (string)
-function decimals() public view returns (uint8)
-function totalSupply() public view returns (uint256)
-function balanceOf(address _owner) public view returns (uint256 balance)
-function transfer(address _to, uint256 _value) public returns (bool success)
-function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
-function approve(address _spender, uint256 _value) public returns (bool success)
-function allowance(address _owner, address _spender) public view returns (uint256 remaining)
+function name() public view returns (string);
+function symbol() public view returns (string);
+function decimals() public view returns (uint8);
+function totalSupply() public view returns (uint256);
+function balanceOf(address _owner) public view returns (uint256 balance);
+function transfer(address _to, uint256 _value) public returns (bool success);
+function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+function approve(address _spender, uint256 _value) public returns (bool success);
+function allowance(address _owner, address _spender) public view returns (uint256 remaining);
 
-event Transfer(address indexed _from, address indexed _to, uint256 _value)
-event Approval(address indexed _owner, address indexed _spender, uint256 _value)
+event Transfer(address indexed _from, address indexed _to, uint256 _value);
+event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
+}
+
+contract Owned{
+    address public owner;
+    address public newOwner;
+
+    event OwnershipTransferred(address indexed _from, address index _to);
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function transferOwnership(address _to) public{
+        require(msg.sender == owner);
+        newOwner = _to;
+    }
+
+    function acceptOwnership() public{
+        require(msg.sender == newOwner);
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
 }
